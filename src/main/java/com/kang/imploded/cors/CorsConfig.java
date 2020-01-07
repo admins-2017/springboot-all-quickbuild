285dcb25c087ba4fce96d7d4cf6b71a366c23a7f
kangdongwei <1067905926@qq.com>
2020-01-07 15:07:18 +0800

    提交实现跨域配置

---
+++ src/main/java/com/kang/imploded/cors/CorsConfig.java
@@ -0,0 +1,35 @@
+package com.kang.imploded.cors;
+
+/**
+ * 实现基本的跨域请求
+ * @author kang
+ * @version 1.0
+ * @date 2020/1/7 14:51
+ */
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        // 允许任何域名使用
+        corsConfiguration.addAllowedOrigin("*");
+        // 允许任何头
+        corsConfiguration.addAllowedHeader("*");
+        // 允许任何方法（post、get等）
+        corsConfiguration.addAllowedMethod("*");
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        // 对接口配置跨域设置
+        source.registerCorsConfiguration("/**", buildConfig());
+        return new CorsFilter(source);
+    }
+}
