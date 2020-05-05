b612de3d506a2ddf75e92eb04a114ff287b1e4ce
admin-2017 <1067905926@qq.com>
2020-05-05 12:13:56 +0800

    测试mac提交

--- src/main/java/com/kang/sys/dto/CommodityDto.java
+++ src/main/java/com/kang/sys/dto/CommodityDto.java
@@ -2,6 +2,7 @@ package com.kang.sys.dto;
 
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import lombok.Setter;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
